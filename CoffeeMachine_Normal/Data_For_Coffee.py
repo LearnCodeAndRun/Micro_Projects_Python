ad8f2d39a14138785fe6bcf841e5fe2e19f4b610
ANEESH DAS <155869164+LearnCodeAndRun@users.noreply.github.com>
2025-08-12 18:29:39 +0530

    Create Data_For_Coffee.py

---
+++ CoffeeMachine_Normal/Data_For_Coffee.py
@@ -0,0 +1,2 @@
+data={"espresso":{"Water":50,"Coffee":18, "Cost":1.5},"latte":{"Water":200,"Coffee":24,"Milk":150,"Cost":2.5},"Cappuccino":{"Water":250,"Coffee":24,"Milk":100,"Cost":3.0}}
+initial={"Water":300,"Milk":200,"Coffee":200,"Money":0}
