8a46c15de4702cebb6fada9728ac24c43bf5363c
ANEESH DAS <155869164+LearnCodeAndRun@users.noreply.github.com>
2025-09-26 18:34:38 +0530

    Update scoreboard.py
    Feature of high score has been added

--- SnakeGame_Day20&21/scoreboard.py
+++ SnakeGame_Day20&21/scoreboard.py
@@ -1,13 +1,23 @@
 # Displaying the scoreboard
 from turtle import Turtle
 class Scoreboard(Turtle):
-    def __init__(self):
+    def __init__(self,filepath):
         super().__init__()
         self.penup()
         self.color("white")
-        self.goto(0,170)
+        self.goto(-90,140)
+        self.penup()
+        self.high_score_record=Turtle()
+        self.high_score_record.penup()
+        self.high_score_record.color("white")
+        self.high_score_record.goto(90,140)
         self.sums=0
+        self.file=filepath
+        with open(self.file,"r") as f:
+           self.high_score=int(f.read())
         self.write(f"Score: {self.sums}",False,"center",("Arial",16,"normal"))
+        self.high_score_record.write(f"High Score: {self.high_score}",False,"center",("Arial",16,"normal"))
+        self.high_score_record.color("black")
         self.color("black")
     def refresh(self):
         self.sums+=3
@@ -15,3 +25,14 @@ class Scoreboard(Turtle):
         self.color("white")
         self.write(f"Score: {self.sums}",False,"center",("Arial",16,"normal"))
         self.color("black")
+        if self.sums>self.high_score:
+            self.high_score=self.sums
+            with open(self.file,"w") as f:
+               f.write(f"{self.high_score}")
+               f.seek(0)
+    def display_high_score(self):
+        with open(self.file,"r") as f:
+          self.high_score=int(f.read())
+        self.high_score_record.clear()
+        self.high_score_record.color("white")
+        self.high_score_record.write(f"High Score: {self.high_score}",False,"center",("Arial",16,"normal"))
