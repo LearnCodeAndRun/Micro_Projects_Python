56933477714544480668cd79c2d8cc1a3fbb9039
ANEESH DAS <155869164+LearnCodeAndRun@users.noreply.github.com>
2026-02-04 13:10:13 +0530

    Refactor Pomodoro GUI layout and styling

--- PomodoroGUI_Day28/main.py
+++ PomodoroGUI_Day28/main.py
@@ -1,16 +1,23 @@
 # Building Pomodoro GUI using Tkinter
 from tkinter import *
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 window=Tk()
-window.minsize(width=600,height=400)
 window.title("Pomodoro")
-window.config(padx=100,pady=50,bg="#f7f5dd")
-window.grid_columnconfigure(0,weight=1)
-window.configure(bg="#f7f5dd")
-tomato=Canvas(width=200,height=224,bg="#f7f5dd",highlightthickness=0)
-text=Label(text="TIMER",font=("Arial",24,"bold"),bg="#f7f5dd",fg="#9bdeac",highlightthickness=0)
-start=Button(text="Start",width=10,highlightthickness=0,)
-reset=Button(text="Reset",width=10,highlightthickness=0)
-text.grid(row=0,column=0,pady=(20,10),columnspan=2)
+window.config(padx=100,pady=50,bg=YELLOW)
+tomato=Canvas(width=200,height=224,bg=YELLOW,highlightthickness=0)
+text=Label(text="TIMER",font=(FONT_NAME,40,"bold"),fg=GREEN,bg=YELLOW)
+start=Button(text="START",font=(FONT_NAME,15,"bold"))
+reset=Button(text="RESET",font=(FONT_NAME,15,"bold"))
+text.grid(column=1,row=0,padx=20,pady=20)
 tomato.grid(column=0,row=1,columnspan=2)
 start.grid(row=3,column=0,padx=20,pady=10)
 reset.grid(row=3,column=1,padx=20,pady=10)
