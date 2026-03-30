df3669386c20cd0a339cb12547d9cb78acaeb4bc
A Das <partialderivative2075@gmail.com>
2026-03-30 22:21:57 +0530

    Exception handling included

--- NATO_phonetic_alphabet/main.py
+++ NATO_phonetic_alphabet/main.py
@@ -1,16 +1,25 @@
 def display(name,student,dic):
-    for (index,row) in student_data_frame.iterrows():
-      if row['student']==name:
-        print(f"Score: {row['score']}")
-    nato_list=[dic[letter.upper()] for letter in name]
-    print(nato_list)
+    try:
+      for (index,row) in student_data_frame.iterrows():
+        if row['student']==name:
+          print(f"Score: {row['score']}")
+      nato_list=[dic[letter.upper()] for letter in name]
+      print(nato_list)
+    except KeyError:
+       print("The given value does not exist")
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
-content=pandas.read_csv('nato_phonetic_alphabet.csv')
+content=pandas.read_csv('./NATO_phonetic_alphabet/nato_phonetic_alphabet.csv')
 dic={row.letter:row.code for (index, row) in content.iterrows()}
-name=input("Enter a name: ").title()
-display(name,student_data_frame,dic)
+try:
+    name=input("Enter a name: ").title()
+    if name.isalpha():
+      display(name,student_data_frame,dic)
+    else:
+      raise ValueError("Input contains non - alphabet characters")
+except ValueError as e:
+   print(f"Error: {e}")
